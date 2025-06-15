d5ed38e095d13b0dff02fe94f3952714a762006a
y n <yn@mbpnoMacBook-Pro.local>
2025-06-15 14:35:53 +0900

    to make PR

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
+		// This test checks if the function returns "even" for an even number HOGEHOGE
 		t.Errorf("Expected 'even', got '%s'", result)
 	}
 }
